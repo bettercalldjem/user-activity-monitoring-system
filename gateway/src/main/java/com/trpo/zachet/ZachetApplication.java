0989ea3cce8d483c8e2fd648dab38cabf38a2ffb
djem djemich <agbtimur@gmail.com>
2025-12-26 12:51:47 +0700

    Add API Gateway (Spring Cloud Gateway, Actuator)

---
+++ gateway/src/main/java/com/trpo/zachet/ZachetApplication.java
@@ -0,0 +1,13 @@
+package com.trpo.zachet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZachetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZachetApplication.class, args);
+	}
+
+}
---
+++ zachet/src/test/java/com/trpo/zachet/ZachetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trpo.zachet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZachetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
